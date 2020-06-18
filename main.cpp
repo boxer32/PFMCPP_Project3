6004138ebe1145e1fbb6ca5bb40a92579737ae11
matkatmusic <matkatmusic@gmail.com>
2020-06-17 20:35:04 -0700

    added part 3

--- main.cpp
+++ main.cpp
@@ -1,80 +1,66 @@
  /*
- Project 3 - Part 2 / 5
- Video: Chapter 2 Part 6
- Implementations tasks
+ Project 3 - Part 3 / 5
+ video: Chapter 2 - Part 8
+ Constructors tasks
+
+ Create a branch named Part3
+
+ On this new branch:
+
+ 1) Add a constructor for each User-Defined-Type.
  
-Create a branch named Part2
+ 2) amend some of your UDT's member functions to print out something interesting via std::cout
+ 
+ 3) Instantiate 1 or 2 instances of each of your user-defined types in the main() function
 
- tasks
- 0) delete all of the plain english pseudo-code you added in Part1.
-   - you should be left with only your UDTs.
-
- 1) write the definition for the Type that leftFoot and rightFoot are instantiations of.
-    don't forget to define and implement the member functions 'stepForward()' and 'stepSize()'
-    you should be able to deduce the return type of those functions based on their usage in Person::run()
-    You'll need to insert the Person struct from the video in the space below.
+ 4) call some of your UDT's amended member functions in main().
+ 
+ 5) add some std::cout statements in main() that print out your UDT's member variable values or values returned from your UDT member functions (if they return values)
+ 
+ After you finish defining each type/function:
+ click the [run] button.  Clear up any errors or warnings as best you can.
+ 
+ example:
  */
 
-struct Foot //fixed
+#include <iostream>
+namespace Example 
 {
-    void stepForward();
-    int stepSize();
-
+struct UDT  // my user defined type named 'UDT'
+{
+    int a; //a member variable
+    UDT();              //1) the constructor
+    void printThing();  //the member function
 };
 
-void Foot::stepForward()
+//the function definitions are outside of the class
+UDT::UDT()
 {
-  // no return
+    a = 0;
 }
 
-int Foot::stepSize()
+void UDT::printThing()
 {
-    return 2;
+    std::cout << "UDT::printThing() " << a << std::endl;  //2) printing out something interesting
 }
 
-struct Person
+int main()
 {
-    int age;
-    int height;
-    float hairLength;
-    float GPA;
-    unsigned int SATScore;
-    int distanceTraveled;
-    Foot leftFoot; 
-    Foot rightFoot;
-
-    void run(int howFast, bool startWithLeftFoot);
-};
-
-void Person::run(int howFast, bool startWithLeftFoot)
-{
-    if (howFast >= 1)
-    {
-        if (startWithLeftFoot == true)
-        {
-            leftFoot.stepForward();
-            rightFoot.stepForward();
-        }
-         else
-        {
-            rightFoot.stepForward();
-            leftFoot.stepForward();
-        }
-        distanceTraveled += leftFoot.stepSize() + rightFoot.stepSize();
-
-    }
-    else
-    {
-     //noting or "return char please insert howFast?"
-    }
+    UDT foo;              //3) instantiating a UDT named 'foo' in main()
+    foo.printThing();     //4) calling a member function of the UDT instance.
+    
+    //5) a std::cout statement accessing foo's member variable.
+    //It also demonstrates a 'ternary expression', which is syntactic shorthand for an 'if/else' expression
+    std::cout << "Is foo's member var 'a' equal to 0? " << (foo.a == 0 ? "Yes" : "No") << "\n";
+    
+    return 0;
 }
- /*
- 2) provide implementations for the member functions you declared in your 10 user-defined types from the previous video outside of your UDT definitions.
- 
- 3) be sure to write the correct full qualified name for the nested type's member functions.
- 
- 4) After you finish defining each type/function, click the [run] button.  Clear up any errors or warnings as best you can.
- */
+} //end namespace Example
+
+//insert Example::main() into main() of user's repo.
+
+
+
 
 //-----------EngineStart
 struct Engine 
@@ -589,5 +575,6 @@ SensorArray AutonomousCar::sensor(SensorArray sensorData)
 #include <iostream>
 int main()
 {
+    Example::main();
     std::cout << "good to go!" << std::endl;
 }
